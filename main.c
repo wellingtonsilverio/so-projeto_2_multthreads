5d8aae75dca86701be051efbc31a368b7def559d
Wellington Silverio <wellington.alves.silverio@gmail.com>
2019-08-09 15:14:23 -0300

    <feat> Criando várias threads

--- main.c
+++ main.c
@@ -14,68 +14,87 @@ void* read_file(void *arg) {
 }
 
 int main() {
-    pthread_t thread_id_1;
-    pthread_t thread_id_2;
-    pthread_t thread_id_3;
+    pthread_t* thread_id;
     void* thread_response;
     int thread_status;
     char* response;
-
+    int i;
+    int n_threads = 16;
     int n_loop = 1000000;
 
-    thread_status = pthread_create(&thread_id_1, NULL, read_file, (void*)(&n_loop));
+    thread_id = malloc(n_threads * sizeof(pthread_t));
 
-    if (thread_status != 0) {
-        printf("Error create!\n");
-        exit(thread_status);
+    for(i = 0; i < n_threads; i++) {
+        thread_status = pthread_create((thread_id + i), NULL, read_file, (void*)(&n_loop));
+
+        if (thread_status != 0) {
+            printf("Error create!\n");
+            exit(thread_status);
+        }
     }
 
-    thread_status = pthread_create(&thread_id_2, NULL, read_file, (void*)(&n_loop));
+    for(i = 0; i < n_threads; i++) {
+        thread_status = pthread_join(*(thread_id + i), &thread_response);
 
-    if (thread_status != 0) {
-        printf("Error create!\n");
-        exit(thread_status);
+        if (thread_status != 0) {
+            printf("Error join!\n");
+            exit(thread_status);
+        }
+
+        response = (char *)thread_response;
+
+        printf("%d) %s\n", i+1, response);
+
     }
 
-    thread_status = pthread_create(&thread_id_3, NULL, read_file, (void*)(&n_loop));
+    free(thread_id);
 
-    if (thread_status != 0) {
-        printf("Error create!\n");
-        exit(thread_status);
-    }
+    // thread_status = pthread_create(&thread_id_2, NULL, read_file, (void*)(&n_loop));
 
-    thread_status = pthread_join(thread_id_1, &thread_response);
+    // if (thread_status != 0) {
+    //     printf("Error create!\n");
+    //     exit(thread_status);
+    // }
 
-    if (thread_status != 0) {
-        printf("Error join!\n");
-        exit(thread_status);
-    }
+    // thread_status = pthread_create(&thread_id_3, NULL, read_file, (void*)(&n_loop));
 
-    response = (char *)thread_response;
+    // if (thread_status != 0) {
+    //     printf("Error create!\n");
+    //     exit(thread_status);
+    // }
 
-    printf("1) %s\n", response);
+    // thread_status = pthread_join(thread_id_1, &thread_response);
 
-    thread_status = pthread_join(thread_id_2, &thread_response);
+    // if (thread_status != 0) {
+    //     printf("Error join!\n");
+    //     exit(thread_status);
+    // }
 
-    if (thread_status != 0) {
-        printf("Error join!\n");
-        exit(thread_status);
-    }
+    // response = (char *)thread_response;
 
-    response = (char *)thread_response;
+    // printf("1) %s\n", response);
 
-    printf("2) %s\n", response);
+    // thread_status = pthread_join(thread_id_2, &thread_response);
 
-    thread_status = pthread_join(thread_id_3, &thread_response);
+    // if (thread_status != 0) {
+    //     printf("Error join!\n");
+    //     exit(thread_status);
+    // }
 
-    if (thread_status != 0) {
-        printf("Error join!\n");
-        exit(thread_status);
-    }
+    // response = (char *)thread_response;
 
-    response = (char *)thread_response;
+    // printf("2) %s\n", response);
 
-    printf("3) %s\n", response);
+    // thread_status = pthread_join(thread_id_3, &thread_response);
+
+    // if (thread_status != 0) {
+    //     printf("Error join!\n");
+    //     exit(thread_status);
+    // }
+
+    // response = (char *)thread_response;
+
+    // printf("3) %s\n", response);
 
     return 0;
 }
