7a49296d461b4947178ae052eab16030b5ee35b4
Wellington Silverio <wellington.alves.silverio@gmail.com>
2019-08-09 15:56:26 -0300

    feat: lendo os argv e arg count

--- main.c
+++ main.c
@@ -6,40 +6,57 @@ void* read_file(void *arg) {
     int i;
     int* loop_count = (int*)arg;
 
-    for (i = 0; i < *loop_count; i++) {
+    for (i = 0; i <= *loop_count; i++) {
         printf("%d\n", i);
     }
 
     pthread_exit("Loop finalizado!");
 }
 
-int main() {
+void thereAnError(int status, char* message) {
+    if (status != 0) {
+        printf("%s\n", message);
+        exit(status);
+    }
+}
+
+int main(int argc, char *argv[]) {
     pthread_t* thread_id;
     void* thread_response;
     int thread_status;
     char* response;
     int i;
     int n_threads = 16;
-    int n_loop = 1000000;
+    char* in_path[1000] = {"arq1.dat", "arq2.dat", "arq3.dat"};
+    char* out_path = "saida.dat";
+
+    int n_loop = 100000;
+
+    if (argc >= 1) n_threads = atoi(argv[1]);
+
+    for (i = 2; i < argc; ++i) {
+        if (argv[i] == "-o") {
+            out_path = argv[i+1];
+            break;
+        }
+
+        in_path[i - 2] = argv[i]; // Tem que arrumar isso aqui
+    }
 
     thread_id = malloc(n_threads * sizeof(pthread_t));
 
+    // Aloca os threads
     for(i = 0; i < n_threads; i++) {
         thread_status = pthread_create((thread_id + i), NULL, read_file, (void*)(&n_loop));
 
-        if (thread_status != 0) {
-            printf("Error create!\n");
-            exit(thread_status);
-        }
+        thereAnError(thread_status, "Error create!");
     }
 
+    // Executa e espera a resposta dos threads
     for(i = 0; i < n_threads; i++) {
         thread_status = pthread_join(*(thread_id + i), &thread_response);
 
-        if (thread_status != 0) {
-            printf("Error join!\n");
-            exit(thread_status);
-        }
+        thereAnError(thread_status, "Error join!");
 
         response = (char *)thread_response;
 
