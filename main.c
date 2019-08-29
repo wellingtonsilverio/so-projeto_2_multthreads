df4b6648d25aa50ee8bbafd26c880c78c388a378
Wellington Silverio <wellington.alves.silverio@gmail.com>
2019-08-28 22:08:31 -0300

    feat: testing and implment banchmark

--- main.c
+++ main.c
@@ -2,25 +2,63 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <string.h>
-#include <time.h>
+#include <sys/time.h>
 
-struct Arg_sort {
+typedef struct {
     int* array;
     int size;
-}Arg_sort;
+} Arg_sort;
 
-struct Arg_populate_matrix {
+typedef struct {
     int amount_files;
     int* matrix_result;
     int* numbers;
-}Arg_populate_matrix;
+} Arg_populate_matrix;
 
-void selectionSort(int* array, int size) {  
+typedef struct {
+  pthread_mutex_t lock;
+  pthread_cond_t wait;
+  int spaces;
+} traffic;
+
+void up(traffic *s) {
+  pthread_mutex_lock(&(s->lock));
+  s->spaces--;
+
+  if (s->spaces < 0) {
+    printf("Não existem vagas disponiveis, aguarde...\n");
+    pthread_cond_wait(&(s->wait), &(s->lock));
+    printf("Vaga liberada!\n");
+  }
+
+  printf("Um carro esta passando, %d vagas disponiveis.\n", s->spaces);
+
+  pthread_mutex_unlock(&(s->lock));
+  return;
+}
+
+// Função para liberar as vagas e mandar o sinal
+// quando foi liberada uma vaga bloqueada
+void down(traffic *s) {
+  pthread_mutex_lock(&(s->lock));
+
+  s->spaces++;
+
+  if (s->spaces <= 0) {
+    pthread_cond_signal(&(s->wait));
+  }
+
+  printf("Uma vaga liberada. %d vagas disponiveis.\n", s->spaces);
+  pthread_mutex_unlock(&(s->lock));
+  return;
+}
+
+void selectionSort(int* array, int size) {
   int i, hasBeenOrganized = 0, min, swap, k = 0;
 
   while(hasBeenOrganized == 0) {
     hasBeenOrganized = 1;
-    for (i = 1; i < size; i++) { 
+    for (i = 1; i < size; i++) {
         if (array[i - 1] > array[i]) {
             swap = array[i];
             array[i] = array[i - 1];
@@ -33,7 +71,8 @@ void selectionSort(int* array, int size) {
 }
 
 void* sort(void* arg) {
-    struct Arg_sort* args = (struct Arg_sort*)arg;
+    Arg_sort* args = (Arg_sort*)arg;
+    int i, j;
 
     selectionSort(args->array, args->size);
 
@@ -41,8 +80,8 @@ void* sort(void* arg) {
 }
 
 void* populateMatrix(void* arg) {
-    struct Arg_populate_matrix* args = (struct Arg_populate_matrix*)arg;
-    
+    Arg_populate_matrix* args = (Arg_populate_matrix*)arg;
+
     int j;
     for (j = 0; j < args->amount_files; j++) {
         args->matrix_result[j] = args->numbers[j];
@@ -78,7 +117,7 @@ int main(int argc, char *argv[]) {
     int i;
     int j;
     int max_n_in_line = 0;
-    int n_threads = 16;
+    int n_threads = 1;
     char* in_path[1000] = {"arq1.dat", "arq2.dat", "arq3.dat"};
     char* out_path = "saida.dat";
     int number_files = 0;
@@ -86,13 +125,19 @@ int main(int argc, char *argv[]) {
     int matrix_result[1000][1000];
     int amount_files[1000];
     int current_thread = 0;
-    float start_time, end_time;
+    struct timeval start_time, end_time;
+    traffic threads;
+    threads.spaces = 1;
+
 
     int n_loop = 100000;
 
     FILE* file;
 
-    if (argc >= 1) n_threads = atoi(argv[1]);
+    if (argc >= 1) {
+            n_threads = atoi(argv[1]);
+            threads.spaces = n_threads;
+    }
 
     for (i = 2; i < argc; ++i) {
         if (strcmp(argv[i],"-o") == 0) {
@@ -134,7 +179,7 @@ int main(int argc, char *argv[]) {
 
     thread_id = malloc(n_threads * sizeof(pthread_t));
 
-    start_time = (float)clock()/CLOCKS_PER_SEC;
+    gettimeofday(&start_time, NULL);
 
     for (i = 0; i < number_files; i++) {
         for(j = 0; j < max_n_in_line; j++) {
@@ -144,16 +189,18 @@ int main(int argc, char *argv[]) {
 
     // Ordena numbers
     for (i = 0; i < number_files; i++) {
-        struct Arg_sort* args = malloc(sizeof(Arg_sort));
+        Arg_sort* args = malloc(sizeof(Arg_sort));
         args->array = numbers[i];
         args->size = sizeof(numbers[i]) / sizeof(int);
 
         thread_status = pthread_create((thread_id + current_thread++), NULL, sort, (void*)(args));
         thereAnError(thread_status, "Error create!");
 
+
         if (current_thread >= n_threads) {
             join_threads(&current_thread, thread_id);
         }
+
     }
     if (current_thread > 0) {
         join_threads(&current_thread, thread_id);
@@ -162,7 +209,7 @@ int main(int argc, char *argv[]) {
     // Adiciona os num na matrix
     if (number_files > 0) {
         for (i = 0; i < number_files; i++) {
-            struct Arg_populate_matrix* args = malloc(sizeof(Arg_populate_matrix));
+            Arg_populate_matrix* args = malloc(sizeof(Arg_populate_matrix));
             args->amount_files = amount_files[i];
             args->matrix_result = matrix_result[i];
             args->numbers = numbers[i];
@@ -180,15 +227,15 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    end_time = (float)clock()/CLOCKS_PER_SEC;
-    
-    printf("Time: %f\n", end_time - start_time);
+    gettimeofday(&end_time, NULL);
+
+    printf("Time: %lld ms\n", (long long)((end_time.tv_sec*1000LL + end_time.tv_usec/1000) - (start_time.tv_sec*1000LL + start_time.tv_usec/1000)));
 
     free(thread_id);
 
     char buffer [50];
     file = fopen(out_path, "w+");
-    
+
     for (i = 0; i < number_files; i++) {
         for(j = 0; j < max_n_in_line; j++) {
             sprintf (buffer, "%d ", matrix_result[i][j]);
